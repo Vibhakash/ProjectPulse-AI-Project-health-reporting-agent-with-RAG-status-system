d40a37122871048f352dc208863069baa80bdd56
Vibha Kashyap <kashyapvib@gmail.com>
2026-08-12 12:02:17 +0530

    Fix: Allow CORS requests from Vercel deployments

--- backend/src/project_health/api.py
+++ backend/src/project_health/api.py
@@ -72,7 +72,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?",
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1)(:\d+)?|https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
